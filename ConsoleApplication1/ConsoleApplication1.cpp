935058fb0c6f89eadee688d91a78f265fe966009
4KT <4KT@DESKTOP-J0KOTS8>
2024-11-11 01:37:50 +0000

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf_s("%d", &n);
+
+    int num1 = 1; 
+    int num2 = 1;  
+
+    
+    while (num1 <= n) {
+        
+        for (int i = 0; i < num2 && num1 <= n; i++) {
+            printf("%d ", num1);
+            num1++;
+        }
+        
+        printf("\n");
+        num2++;
+    }
+}
